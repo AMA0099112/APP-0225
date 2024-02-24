5844b04de31b5c8bcf062893d6ab10413daf3a00
AMA0099112 <99637348+AMA0099112@users.noreply.github.com>
2024-02-25 00:54:24 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>數位n0 甲 陳怡蒨</Text>
+      <Text>111119022</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
